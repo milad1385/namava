bba52520956df9db84b2b51963250a317f1c6935
Milad salamian <miladsalami1385@gmail.com>
2025-05-15 15:27:13 -0700

    add next js top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import Navbar from "@/src/components/modules/Navbar/Navbar";
 import FooterMenu from "@/src/components/modules/FooterMenu/FooterMenu";
 import Footer from "@/src/components/modules/Footer/Footer";
@@ -7,6 +6,8 @@ import { Toaster } from "react-hot-toast";
 import { authUser } from "@/src/utils/serverHelper";
 import { checkUserSubscription } from "../libs/service/services";
 import AuthContextProvider from "../context/AuthContextProvider";
+import "@/src/globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: {
@@ -31,6 +32,7 @@ export default async function RootLayout({
   return (
     <html lang="fa" dir="rtl" className="font-Iran bg-[#121212]">
       <body>
+        <NextTopLoader height={5} color="#0066ff" showSpinner={false} />
         <AuthContextProvider>
           <Navbar
             user={JSON.parse(JSON.stringify(user))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,10 +30,8 @@ const config: Config = {
         yellow: "0 0px 5px #FFA41B",
       },
       colors: {
-        namava: "#1993ff",
-        namavaBlack: "#222327",
-        customGray: "#ccc",
-        namavaGreen : "rgb(153, 193, 77)"
+        primary: "#EAB308",
+        namavaBlack :"#1A1A1A"
       },
     },
     fontFamily: {
