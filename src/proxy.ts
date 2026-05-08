62af0aec3291632955dcf46342aa2eaa62956293
Milad salamian <miladsalami1385@gmail.com>
2026-05-08 18:56:05 +0330

    add proxy.ts file

---
+++ src/proxy.ts
@@ -0,0 +1,38 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken");
+  const pathname = request.nextUrl.pathname;
+
+
+  if (
+    pathname.startsWith("/plans") ||
+    pathname.startsWith("/p-user") ||
+    pathname.startsWith("/profile-list-edit")
+  ) {
+    if (!accessToken) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    } else {
+      NextResponse.next();
+    }
+  }
+
+  if (pathname.startsWith("/login") || pathname.startsWith("/register")) {
+    if (accessToken) {
+      return NextResponse.redirect(new URL("/", request.url));
+    } else {
+      NextResponse.next();
+    }
+  }
+}
+
+export const config = {
+  matcher: [
+    "/plans/:path*",
+    "/p-user/:path*",
+    "/login",
+    "/register",
+    "/profile-list-edit/:path*",
+  ],
+};
