c6bef1e8e9e3e80a4ca4634b645ef283c9266cee
Milad salamian <miladsalami1385@gmail.com>
2026-05-10 19:07:53 +0330

    fix movie component bug

--- src/components/modules/main/Movie/Movie.tsx
+++ src/components/modules/main/Movie/Movie.tsx
@@ -14,48 +14,67 @@ function Movie({
   type,
   showTime,
 }: IMovie) {
+  console.log(isLink);
+
   const router = useRouter();
   const pathname = usePathname();
-  const handleMovieNavigation = async () => {
-    if (isLink) {
-      if (type === "film") {
-        if (contentType === "adult") {
-          router.push(`/movie/${link}`);
-        } else {
-          router.push(`/kids/movie/${link}`);
-        }
-      } else {
-        if (contentType === "adult") {
-          router.push(`/series/${link}`);
-        } else {
-          router.push(`/kids/series/${link}`);
-        }
-      }
+
+  const getHref = () => {
+    if (type === "film") {
+      return !isLink ? `/movie/${link}` : `/kids/movie/${link}`;
+    } else {
+      return !isLink ? `/series/${link}` : `/kids/series/${link}`;
     }
   };
+
+  const handleMovieNavigation = () => {
+    if (isLink) {
+      router.push(getHref());
+    }
+  };
+
+  const Wrapper = ({ children }) => {
+    if (isLink) {
+      return (
+        <Link
+          href={getHref()}
+          className="transition-all hidden lg:block group cursor-pointer"
+        >
+          {children}
+        </Link>
+      );
+    } else {
+      return (
+        <div
+          onClick={handleMovieNavigation}
+          className="transition-all hidden lg:block group cursor-pointer"
+        >
+          {children}
+        </div>
+      );
+    }
+  };
+
   return (
     <>
-      <div
-        onClick={() => handleMovieNavigation()}
-        className={`transition-all hidden lg:block group cursor-pointer `}
-      >
+      <Wrapper>
         <div className="relative">
           <Image
             src={image}
-            alt="slide1.jpg"
+            alt={title}
             width={1920}
             height={1080}
             className={`rounded-md shadow w-full lg:w-full lg:h-[280px] object-cover md:w-auto h-[160px] md:h-auto ${
               pathname.includes("/kids/collections") ? "lg:h-[401px]" : ""
             }`}
           />
-          <div className="flex !text-white text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible  duration-100 opacity-0 invisible  justify-end flex-col absolute inset-0  movie-overlay rounded-md">
+          <div className="flex !text-white text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible duration-100 opacity-0 invisible justify-end flex-col absolute inset-0 movie-overlay rounded-md">
             <div className="pb-6 px-2 space-y-2 md:space-y-3">
               <p className="text-xs md:text-[13px]">
                 {type === "film" ? "فیلم" : "سریال"} -{" "}
                 <span className="font-Dana">{showTime}</span>
               </p>
-              <div className="flex text-xs md:text-[13px]  gap-x-1">
+              <div className="flex text-xs md:text-[13px] gap-x-1">
                 <Heart />
                 <span className="font-Dana mt-[1px]">%80</span>
               </div>
@@ -63,16 +82,17 @@ function Movie({
           </div>
         </div>
         <h3
-          className={`text-xs  mt-3 mr-3 ${
+          className={`text-xs mt-3 mr-3 ${
             pathname.includes("kids") ? "text-black !text-base" : "text-white"
           }`}
         >
           {title}
         </h3>
-      </div>
+      </Wrapper>
+
       <Link
-        href={type === "film" ? `/movie/${link}` : `/series/${link}`}
-        className={`transition-all block lg:hidden group cursor-pointer `}
+        href={getHref()}
+        className="transition-all block lg:hidden group cursor-pointer"
       >
         <div className="relative">
           <Image
@@ -82,13 +102,13 @@ function Movie({
             height={500}
             className="rounded-md w-full lg:w-full lg:h-[270px] object-cover md:w-auto h-[160px] md:h-auto"
           />
-          <div className="flex text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible  duration-100 opacity-0 invisible  justify-end flex-col absolute inset-0  movie-overlay rounded-md">
+          <div className="flex text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible duration-100 opacity-0 invisible justify-end flex-col absolute inset-0 movie-overlay rounded-md">
             <div className="pb-6 px-2 space-y-2 md:space-y-3">
               <p className="text-xs md:text-[13px]">
                 {type === "film" ? "فیلم" : "سریال"} -{" "}
                 <span className="font-Dana">{showTime}</span>
               </p>
-              <div className="flex  gap-x-1">
+              <div className="flex gap-x-1">
                 <Heart />
                 <span className="font-Dana mt-[1px]">%80</span>
               </div>
