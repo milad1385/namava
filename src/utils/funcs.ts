1fff841eceb8d44549d49f8e8f97f5aa6e737d1e
Milad salamian <miladsalami1385@gmail.com>
2026-06-08 00:06:09 +0330

    fix prepareData func

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -17,7 +17,6 @@ export const getRemainingDays = (user: any) => {
 
 export function prepareData(startData: any, orders: any) {
   // A bit ugly code, but sometimes this is what it takes when working with real data 😅
-
   function incArrayValue(arr: any, field: any) {
     return arr.map((obj: any) =>
       obj.duration === field ? { ...obj, value: obj.value + 1 } : obj,
@@ -26,7 +25,7 @@ export function prepareData(startData: any, orders: any) {
 
   const data = orders
     .reduce((arr: any, cur: any) => {
-      const num = cur.time;
+      const num = cur.subscription.time;
       if (num === 30) return incArrayValue(arr, "اشتراک 30 روزه");
       if (num === 60) return incArrayValue(arr, "اشتراک 60 روزه");
       if (num === 90) return incArrayValue(arr, "اشتراک 90 روزه");
