01ac50ccd8445d0fdcdd859419e3dc05a67e36db
Milad salamian <miladsalami1385@gmail.com>
2026-06-07 23:34:26 +0330

    generate order detail in order detail page

--- src/app/p-user/orders/[id]/page.tsx
+++ src/app/p-user/orders/[id]/page.tsx
@@ -1,7 +1,14 @@
 import Button from "@/src/components/modules/auth/Button/Button";
-import React from "react";
+import { getOrder } from "@/src/libs/service/services";
+import { TParams } from "@/src/libs/types";
+import { getDateWithTime } from "@/src/utils/funcs";
+import Link from "next/link";
 
-function page() {
+async function page({ params }: TParams) {
+  const order = await getOrder(params.id as string);
+  const paidTime = getDateWithTime(order.paid_time);
+  const orderStatusStyle =
+    order.status === "pay" ? "text-green-600" : "text-red-600";
   return (
     <div className="bg-namavaBlack w-full px-[28] rounded-md py-6 md:w-1/2 mx-auto text-white">
       <h1 className="text-base md:text-lg font-IranMedium text-center">
@@ -10,40 +17,46 @@ function page() {
       <ul className="w-full mt-5 space-y-4 border-b border-b-slate-500 pb-4">
         <li className="flex items-center justify-between">
           <span>شرح : </span>
-          <span>یک ماهه</span>
+          <span>{order.subscription.title}</span>
         </li>
         <li className="flex items-center justify-between">
           <span>شماره سفارش : </span>
-          <span>۱۴۰۳۰۵۰۴-۱۷۳۳-۲۹۳۷</span>
+          <span className="font-Dana">{order.orderNumber}</span>
         </li>
         <li className="flex items-center justify-between">
           <span>زمان و ثبت سفارش : </span>
-          <span>۱۴۰۳/۵/۴ ۰۷:۰۳:۵۶</span>
+          <span>{paidTime}</span>
         </li>
         <li className="flex items-center justify-between">
           <span>وضعیت سفارش : </span>
-          <span className="text-red-700">ناموفق</span>
+          <span className={orderStatusStyle}>
+            {order.status === "pay" ? "موفق" : "ناموفق"}
+          </span>
         </li>
       </ul>
       <ul className="w-full mt-5 space-y-4">
         <li className="flex items-center justify-between">
           <span>قیمت : </span>
-          <span>۱۶۰,۰۰۰ تومان</span>
+          <span>
+            {(order.totalPrice + order.discount).toLocaleString("fa-IR")} تومان
+          </span>
         </li>
-        <li className="flex items-center justify-between text-red-700">
+        <li className={`flex items-center justify-between ${orderStatusStyle}`}>
           <span>جمع تخفیف : </span>
-          <span>۱۶۰,۰۰۰ تومان</span>
+          <span>{order.discount.toLocaleString("fa-IR")} تومان</span>
         </li>
-        <li className="flex items-center justify-between">
-          <span>مالیات بر ارزش افزوده : </span>
-          <span>۱۱,۲۰۰ تومان</span>
-        </li>
-        <li className="flex items-center justify-between text-namava">
+        <li
+          className={`flex items-center justify-between ${order.status === "pay" ? "text-namava" : "text-red-600"}`}
+        >
           <span>مبلغ پرداختی : </span>
-          <span>۱۲۳,۲۰۰ تومان</span>
+          <span>{order.totalPrice.toLocaleString("fa-IR")} تومان</span>
         </li>
-
-        <Button>بازگشت</Button>
+        <Link
+          href="/p-user/orders"
+          className={`block mt-10 ${order.status === "pay" ? "" : "!bg-red-600"}`}
+        >
+          <Button>بازگشت</Button>
+        </Link>
       </ul>
     </div>
   );
