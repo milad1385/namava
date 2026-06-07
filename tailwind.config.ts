9ec814fed680efac8876439757c9772fc0a5c30d
Milad salamian <miladsalami1385@gmail.com>
2026-06-07 22:26:46 +0330

    fix namavaGreen color in tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,7 +31,8 @@ const config: Config = {
       },
       colors: {
         namava: "#1993ff",
-        namavaBlack :"#1A1A1A"
+        namavaBlack :"#1A1A1A",
+        namavaGreen :"#15bc15"
       },
     },
     fontFamily: {
