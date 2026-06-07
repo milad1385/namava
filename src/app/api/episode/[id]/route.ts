017d873df1eafb32f6692e53db57acb6051f8612
Milad salamian <miladsalami1385@gmail.com>
2026-06-07 12:17:33 +0330

    add get episode by series id route

---
+++ src/app/api/episode/[id]/route.ts
@@ -0,0 +1,16 @@
+import connectToDB from "@/src/configs/db";
+import { TParams } from "@/src/libs/types";
+import EpisodeModel from "@/src/models/episode";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest, { params }: TParams) {
+  try {
+    connectToDB();
+    const episode = await EpisodeModel.findOne({
+      series: params.id,
+    });
+    return Response.json(episode);
+  } catch (err) {
+    return Response.json({ err: "interval server err" }, { status: 500 });
+  }
+}
