502ca9ae1b9553069a6bd30eef44a349d7566473
Milad salamian <miladsalami1385@gmail.com>
2026-06-08 23:45:35 +0330

    check profile type when user in main , movie and series page

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ export async function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken");
   const profileId = request.cookies.get("profile")?.value;
   const pathname = request.nextUrl.pathname;
-
   if (
     pathname.startsWith("/plans") ||
     pathname.startsWith("/p-user") ||
@@ -16,6 +15,44 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/login", request.url));
     } else {
       NextResponse.next();
+    }
+  }
+
+  if (
+    pathname === "/" ||
+    pathname.includes("/movie") ||
+    pathname.includes("/series")
+  ) {
+    if (profileId && accessToken) {
+      try {
+        const res = await fetch(
+          `http://localhost:3000/api/profile?id=${profileId}`,
+          {
+            headers: {
+              Cookie: request.headers.get("cookie") || "",
+            },
+          },
+        );
+
+        if (!res.ok) {
+          console.error("Failed to fetch profile");
+          return NextResponse.next();
+        }
+
+        const data = await res.json();
+        console.log("Profile type on home page:", data?.profile?.type);
+
+        // اگه نوع پروفایل بزرگسال نیست (یعنی کودک هست) و مسیر kids نیست
+        if (data?.profile?.type !== "adult") {
+          return NextResponse.redirect(new URL("/kids", request.url));
+        }
+
+        // اگه بزرگسال هست، اجازه بده بمونه تو صفحه اصلی
+        return NextResponse.next();
+      } catch (error) {
+        console.error("Middleware error on home page:", error);
+        return NextResponse.next();
+      }
     }
   }
 
@@ -30,10 +67,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    "/",
     "/plans/:path*",
     "/p-user/:path*",
     "/login",
     "/register",
     "/profile-list-edit/:path*",
+    "/movie/:path*",
+    "/series/:path*",
   ],
 };
