a1951d977119491e7b589c6da5f01174fda60349
Milad salamian <miladsalami1385@gmail.com>
2026-06-11 02:09:47 +0330

    add limited route in chat button component

--- src/components/modules/chatbot/ChatButton.tsx
+++ src/components/modules/chatbot/ChatButton.tsx
@@ -3,13 +3,20 @@
 import { useState } from "react";
 import { FaRobot, FaTimes } from "react-icons/fa";
 import ChatWidget from "./ChatWidget";
+import { usePathname } from "next/navigation";
+import { limitedRoute } from "@/public/db";
 
 export default function ChatButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const result = limitedRoute.some((route) => pathname.includes(route));
+  if (result) {
+    return null;
+  }
 
   return (
     <>
-      {/* دکمه دایره‌ای پایین سمت راست */}
       <button
         onClick={() => setIsOpen(!isOpen)}
         className={`fixed bottom-6 right-6 z-50 w-14 h-14 rounded-full bg-blue-600 text-white shadow-2xl hover:bg-blue-700 transition-all duration-300 flex items-center justify-center ${
@@ -19,7 +26,6 @@ export default function ChatButton() {
         {isOpen ? <FaTimes size={24} /> : <FaRobot size={24} />}
       </button>
 
-      {/* کارت چت که با کلیک روی دکمه باز/بسته می‌شود */}
       {isOpen && <ChatWidget onClose={() => setIsOpen(false)} />}
     </>
   );
