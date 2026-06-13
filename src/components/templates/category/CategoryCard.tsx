2ab49a31d0c7a4111418420244c5365a2131d0bf
Milad salamian <miladsalami1385@gmail.com>
2026-06-14 01:02:22 +0330

    add ICategoryCard in category card component

--- src/components/templates/category/CategoryCard.tsx
+++ src/components/templates/category/CategoryCard.tsx
@@ -1,28 +1,35 @@
+import { ICategoryCard } from "@/src/libs/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-interface CategoryCard {
-  title: string;
-  image: string;
-  link: string;
-}
-
-function CategoryCard({ title, image, link }: CategoryCard) {
-  
-  
+function CategoryCard({ title, image, link }: ICategoryCard) {
   return (
     <div className="relative flex items-center">
-      <Link href={`${link}`}>
-        <Image
-          src={image}
-          alt={image}
-          width={900}
-          height={900}
-          className="rounded-md"
-        />
-      </Link>
-      <span className="text-sm category-title md:text-xl lg:text-2xl absolute right-3 md:right-6 max-w-[73.9px] md:max-w-[190px]">{title}</span>
+      {link ? (
+        <Link href={`${link}`}>
+          <Image
+            src={image}
+            alt={image}
+            width={900}
+            height={900}
+            className="rounded-md"
+          />
+        </Link>
+      ) : (
+        <div className="md:cursor-pointer">
+          <Image
+            src={image}
+            alt={image}
+            width={900}
+            height={900}
+            className="rounded-md"
+          />
+        </div>
+      )}
+      <span className="text-sm category-title md:text-xl lg:text-2xl absolute right-3 md:right-6 max-w-[73.9px] md:max-w-[190px]">
+        {title}
+      </span>
     </div>
   );
 }
