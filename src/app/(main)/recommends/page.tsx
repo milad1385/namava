d69e02c93eb027e4e15d1ac95fda4bc26d43b80a
Milad salamian <miladsalami1385@gmail.com>
2026-06-14 01:02:02 +0330

    add meta data in recommend page

--- src/app/(main)/recommends/page.tsx
+++ src/app/(main)/recommends/page.tsx
@@ -1,7 +1,22 @@
+import Recommends from "@/src/components/templates/recommends/Recommends";
+import { getCategories } from "@/src/libs/service/services";
+import { Metadata } from "next";
 import React from "react";
 
-function page() {
-  return <div>page</div>;
+export const metadata: Metadata = {
+  title: "پیشنهاد فیلم و سریال",
+  description:
+    "در این صفحه میتوانید با انتخاب ژانر های خود ، از هوش مصنوعی پیشنهادات تماشا فیلم و سریال را دریافت کنید",
+  keywords: "پیشنهاد ، فیلم ، هوش مصنوعی ، سریال",
+};
+
+async function page() {
+  const categories = await getCategories();
+  return (
+    <div className="my-28 text-white container">
+      <Recommends categories={JSON.parse(JSON.stringify(categories))} />
+    </div>
+  );
 }
 
 export default page;
