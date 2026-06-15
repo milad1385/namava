fe3232ca9b8466ddf03342224d00bf28af53cc92
Milad salamian <miladsalami1385@gmail.com>
2026-06-15 22:21:27 +0330

    fix movie component style bug

--- src/components/modules/main/Movie/Movie.tsx
+++ src/components/modules/main/Movie/Movie.tsx
@@ -102,7 +102,7 @@ function Movie({
             alt={title}
             width={490}
             height={500}
-            className="rounded-md w-full lg:w-full lg:h-[270px] object-cover md:w-auto h-[160px] md:h-auto"
+            className="rounded-md w-full lg:w-full lg:h-[270px] object-cover md:w-auto h-[170px] md:h-auto"
           />
           <div className="flex text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible duration-100 opacity-0 invisible justify-end flex-col absolute inset-0 movie-overlay rounded-md">
             <div className="pb-6 px-2 space-y-2 md:space-y-3">
