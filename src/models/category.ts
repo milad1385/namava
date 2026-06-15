31cae56a278d80e0c71570e9fe69678bafe12afa
Milad salamian <miladsalami1385@gmail.com>
2026-06-15 18:19:19 +0330

    fix category model bug

---
+++ src/models/category.ts
@@ -0,0 +1,55 @@
+import mongoose, { Schema, models, Model } from "mongoose";
+
+export interface ICategory {
+  title: string;
+  link?: string;
+  description: string;
+  tags: string[];
+  parrent?: Schema.Types.ObjectId | null;
+  image?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const schema = new Schema<ICategory>(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    link: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    tags: [
+      {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    ],
+    parrent: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      default: null,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true },
+);
+
+// 📌 تعریف مدل با تایپ صحیح
+const CategoryModel: Model<ICategory> =
+  models.Category || mongoose.model<ICategory>("Category", schema);
+
+export default CategoryModel;
