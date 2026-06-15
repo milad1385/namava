c6aa77e913b2ffb768e50f6e141db51ee89efa68
Milad salamian <miladsalami1385@gmail.com>
2026-06-15 18:21:17 +0330

    fix actor model bug

---
+++ src/models/actor.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, models, Model } from "mongoose";
+
+export interface IActor {
+  name: string;
+  biography: string;
+  link: string;
+  twitter?: string;
+  instagram?: string;
+  image?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const schema = new Schema<IActor>(
+  {
+    name: { type: String, required: true, trim: true },
+    biography: { type: String, required: true, trim: true },
+    link: { type: String, required: true, trim: true },
+    twitter: { type: String, trim: true },
+    instagram: { type: String, trim: true },
+    image: {
+      type: String,
+      trim: true,
+      default: "/uploads/star-placeholder.png",
+    },
+  },
+  { timestamps: true },
+);
+
+const ActorModel: Model<IActor> =
+  models.Actor || mongoose.model<IActor>("Actor", schema);
+
+export default ActorModel;
