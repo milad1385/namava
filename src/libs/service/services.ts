e4d84c46f9620769a03051e10bbf5fd51605316b
Milad salamian <miladsalami1385@gmail.com>
2026-06-15 19:16:02 +0330

    fix getUserBookmarks service bug

--- src/libs/service/services.ts
+++ src/libs/service/services.ts
@@ -858,7 +858,7 @@ export const getUserBookmarks = async () => {
       "link title mainImage type showTime contentType",
     );
 
-    return bookmarks;
+    return bookmarks || [];
   } catch (error) {
     return error;
   }
