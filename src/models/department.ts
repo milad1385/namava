634e00d879ba907effb8c7356552b483e69b8c44
Milad salamian <miladsalami1385@gmail.com>
2026-06-15 18:29:02 +0330

    fix department model bug

---
+++ src/models/department.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IDepartment {
+  title: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const schema = new Schema<IDepartment>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.models.Department || mongoose.model<IDepartment>("Department", schema);
+
+export default model;
