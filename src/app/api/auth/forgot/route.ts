ee9479df1d0dc7bb45c40bef98e50a2e764d64d4
Milad salamian <miladsalami1385@gmail.com>
2026-06-21 22:23:06 +0330

    fix send reset email bug

--- src/app/api/auth/forgot/route.ts
+++ src/app/api/auth/forgot/route.ts
@@ -1,13 +1,21 @@
-// src/app/api/auth/forgot/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import { sendResetEmail , generateResetToken } from "@/src/libs/mailer";
-
+import { sendResetEmail, generateResetToken } from "@/src/libs/mailer";
+import UserModel from "@/src/models/user";
 export async function POST(request: NextRequest) {
   try {
     const { email } = await request.json();
 
     if (!email) {
       return NextResponse.json({ error: "ایمیل الزامی است" }, { status: 400 });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "کاربری با این ایمیل یافت نشد" },
+        { status: 404 },
+      );
     }
 
     const token = generateResetToken(email);
@@ -23,5 +31,3 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "خطا در ارسال ایمیل" }, { status: 500 });
   }
 }
-
-
