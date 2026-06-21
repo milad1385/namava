938a0906684a0f34aad1b21b82aa59c9f3843761
Milad salamian <miladsalami1385@gmail.com>
2026-06-21 19:18:47 +0330

    add reset password page

---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,33 @@
+import ChangePassword from "@/src/components/templates/auth/forgot/ChangePassword";
+
+import Logo from "@/src/icons/Logo";
+import { Metadata } from "next";
+import Link from "next/link";
+import React from "react";
+
+export type ForgotProps = {
+  searchParams?: { [key: string]: string | string[] | undefined };
+};
+
+export const metadata: Metadata = {
+  title: "ایجاد رمز جدید",
+};
+
+function ResetPassword() {
+  return (
+    <div className="bg-milafilmBlack md:bg-[#121212] flex items-center justify-center min-h-screen text-white">
+      <div className="login-form relative  md:shadow w-[500px] max-h-[641px] bg-milafilmBlack px-[40px] md:px-[60px] py-[20px] md:py-[30px] rounded-lg">
+        <Logo className="fill-milafilm !w-[96px] !h-[61px] mx-auto" />
+        <Link
+          href={"/login"}
+          className="text-milafilm absolute left-10 md:left-20 top-10 md:top-12"
+        >
+          ورود
+        </Link>
+        <ChangePassword />
+      </div>
+    </div>
+  );
+}
+
+export default ResetPassword;
