c713edb1d4386a0acc5d72ffab7a623cc70f8e8f
Milad salamian <miladsalami1385@gmail.com>
2026-06-21 22:23:39 +0330

    add resetPasswordHandler

--- src/components/templates/auth/forgot/ChangePassword.tsx
+++ src/components/templates/auth/forgot/ChangePassword.tsx
@@ -3,16 +3,21 @@ import Button from "@/src/components/modules/auth/Button/Button";
 import Input from "@/src/components/modules/auth/Input/Input";
 import Label from "@/src/components/modules/auth/Label/Label";
 import Desc from "@/src/components/modules/auth/desc/desc";
+import Spinner from "@/src/components/modules/spinner/Spinner";
 import Remember from "@/src/icons/Remember";
 import { PasswordFormData, passwordSchema } from "@/src/validators/frontend";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 
 function ChangePassword({ token }: { token: string }) {
   const [isShowPass, setIsShowPassord] = useState(true);
   const [isShowConfirmPass, setIsShowConfirmPassord] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const {
     register,
@@ -30,7 +35,28 @@ function ChangePassword({ token }: { token: string }) {
   const confirmPassword = watch("confirmPassword");
 
   const resetPasswordHandler = async (data: PasswordFormData) => {
-    console.log(data);
+    try {
+      setIsLoading(true);
+      const response = await fetch("/api/auth/forgot/reset-password", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          token,
+          newPassword: data.password,
+        }),
+      });
+
+      const result = await response.json();
+      setIsLoading(false);
+      if (!response.ok) {
+        toast.error("خطا در تغییر رمز عبور");
+      }
+
+      toast.success("رمز عبور با موفقیت تغییر کرد");
+      return router.replace("/login");
+    } catch (error: any) {
+      console.error(error.message);
+    }
   };
 
   const handleFieldChange = (field: keyof PasswordFormData, value: string) => {
@@ -61,6 +87,7 @@ function ChangePassword({ token }: { token: string }) {
               value={password || ""}
               onChange={(e) => handleFieldChange("password", e.target.value)}
               dir={password ? "ltr" : "rtl"}
+              disabled={isLoading}
               placeholder="رمز عبور جدید را وارد کنید."
               className={password ? "text-left" : "text-right"}
             />
@@ -91,6 +118,7 @@ function ChangePassword({ token }: { token: string }) {
             <Input
               type={isShowConfirmPass ? "password" : "text"}
               value={confirmPassword || ""}
+              disabled={isLoading}
               onChange={(e) =>
                 handleFieldChange("confirmPassword", e.target.value)
               }
@@ -117,7 +145,9 @@ function ChangePassword({ token }: { token: string }) {
           )}
         </div>
 
-        <Button className="!mt-10" type="submit">تایید</Button>
+        <Button className="!h-[50px] !mt-10" disabled={isLoading}>
+          {isLoading ? <Spinner /> : "ارسال رمز عبور"}
+        </Button>
       </form>
     </>
   );
