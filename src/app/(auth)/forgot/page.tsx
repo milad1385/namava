88e22683fd699069333b2492ee0493ed2f4394b8
Milad salamian <miladsalami1385@gmail.com>
2026-06-21 18:12:58 +0330

    fix forgot password page

--- src/app/(auth)/forgot/page.tsx
+++ src/app/(auth)/forgot/page.tsx
@@ -1,7 +1,8 @@
 import ChangePassword from "@/src/components/templates/auth/forgot/ChangePassword";
-import SendPhone from "@/src/components/templates/auth/forgot/SendPhone";
+import SendEmail from "@/src/components/templates/auth/forgot/SendEmail";
 import VerifyOtp from "@/src/components/templates/auth/forgot/VerifyOtp";
 import Logo from "@/src/icons/Logo";
+import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
 
@@ -9,7 +10,11 @@ export type ForgotProps = {
   searchParams?: { [key: string]: string | string[] | undefined };
 };
 
-function ForgotPage({ searchParams }: ForgotProps) {
+export const metadata: Metadata = {
+  title: "فراموشی رمز ، ارسال درخواست",
+};
+
+function ForgotPage() {
   return (
     <div className="bg-milafilmBlack md:bg-[#121212] flex items-center justify-center min-h-screen text-white">
       <div className="login-form relative  md:shadow w-[500px] max-h-[641px] bg-milafilmBlack px-[40px] md:px-[60px] py-[20px] md:py-[30px] rounded-lg">
@@ -20,34 +25,10 @@ function ForgotPage({ searchParams }: ForgotProps) {
         >
           ثبت نام
         </Link>
-        {searchParams?.type === "verify" && (
-          <>
-            <VerifyOtp />
-            <div className="flex-center flex-col text-xs md:text-sm text-milafilm space-y-6 !mt-8">
-              <Link href={"?type=forgot"}>شماره را اشتباه وارد کردید ؟</Link>
-            </div>
-          </>
-        )}
-        {searchParams?.type === "finally" && <ChangePassword />}
-        {(!searchParams?.type || searchParams?.type === "phone") && (
-          <SendPhone />
-        )}
+        <SendEmail />
       </div>
     </div>
   );
-}
-
-export async function generateMetadata({ searchParams }: ForgotProps) {
-  const registerType =
-    searchParams?.type === "verify"
-      ? "کد یکبار مصرف"
-      : searchParams?.type === "finally"
-      ? "رمز عبور جدید"
-      : "شماره همراه";
-
-  return {
-    title: `${registerType} را وارد کنید`,
-  };
 }
 
 export default ForgotPage;
