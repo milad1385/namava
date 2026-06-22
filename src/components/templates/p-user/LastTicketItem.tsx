b82215094e624ca0add8b36d12c3057c57726328
Milad salamian <miladsalami1385@gmail.com>
2026-06-22 13:43:10 +0330

    fix last ticket item style bug

--- src/components/templates/p-user/LastTicketItem.tsx
+++ src/components/templates/p-user/LastTicketItem.tsx
@@ -9,15 +9,23 @@ function LastTicketItem({ ticket }: { ticket: ILastTicket }) {
       href={`/p-user/tickets/${ticket._id}`}
       className="text-white bg-[#121212] p-4 rounded-md flex items-center justify-between"
     >
-      <div className="space-y-3">
+      <div className="space-y-3 text-sm md:text-base">
         <h4>{ticket.title}</h4>
-        <div className="bg-white w-[110px] text-[#121212] flex-center py-2 rounded-md">
+        <div className="bg-white text-xs md:text-sm w-[90px] text-[#121212] flex-center py-2 rounded-md">
           {ticket.department.title}
         </div>
       </div>
       <div className="text-center space-y-3">
-        <h5 className="font-Dana">{formatDate(ticket.createdAt)}</h5>
-        <p>پاسخ داده {ticket.isAnswer ? "شده" : "نشده"}</p>
+        <h5 className="font-Dana  text-sm md:text-base">
+          {formatDate(ticket.createdAt)}
+        </h5>
+        <p
+          className={`text-xs md:text-sm ${
+            ticket.status === "answered" ? "text-green-600" : "text-red-600"
+          }`}
+        >
+          پاسخ داده {ticket.status === "answered" ? "شده" : "نشده"}
+        </p>
       </div>
     </Link>
   );
