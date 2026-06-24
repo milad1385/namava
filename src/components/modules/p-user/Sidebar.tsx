48a822de622d8ee27ecc59302a1033d11590f69c
Milad salamian <miladsalami1385@gmail.com>
2026-06-24 19:12:43 +0330

    fix sidebar component bug

--- src/components/modules/p-user/Sidebar.tsx
+++ src/components/modules/p-user/Sidebar.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { FiLogOut } from "react-icons/fi";
-import SidebarLink from "./SidebarLink";
 import { authUser } from "@/src/utils/serverHelper";
+import Image from "next/image";
 import Logout from "../../templates/p-user/Logout";
+import SidebarLink from "./SidebarLink";
 
 async function Sidebar() {
   const user = await authUser();
@@ -10,10 +9,12 @@ async function Sidebar() {
     <div className="hidden lg:block w-[480px] h-[565px] bg-milafilmBlack text-white shadow  rounded-md p-4 sticky top-0">
       <div className="flex items-center gap-x-4 border-b-2 border-b-gray-500 pb-2">
         <div className="w-[55px] h-[55px] flex-center rounded-full">
-          <img
+          <Image
             src={`/images/user.png`}
             alt="user.png"
             className="w-[50px] h-[50px] rounded-full"
+            width={1920}
+            height={1080}
           />
         </div>
         <div className="flex  flex-col gap-y-1 font-DanaDemiBold">
