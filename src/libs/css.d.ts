466088d9ab06522f1e4409120a74e68a06c16dd0
Milad salamian <miladsalami1385@gmail.com>
2026-06-24 22:50:51 +0330

    fix css imports bugs

---
+++ src/libs/css.d.ts
@@ -0,0 +1,34 @@
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "swiper/css" {
+  const content: any;
+  export default content;
+}
+
+declare module "swiper/css/navigation" {
+  const content: any;
+  export default content;
+}
+
+declare module "swiper/css/pagination" {
+  const content: any;
+  export default content;
+}
+
+declare module "swiper/css/autoplay" {
+  const content: any;
+  export default content;
+}
+
+declare module "swiper/css/effect-fade" {
+  const content: any;
+  export default content;
+}
+
+declare module "swiper/css/scrollbar" {
+  const content: any;
+  export default content;
+}
