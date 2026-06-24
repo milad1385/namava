c26d8a74fd929282c7b132e6563838dc136df98f
Milad salamian <miladsalami1385@gmail.com>
2026-06-24 20:41:33 +0330

    fix movie style bug

--- src/components/modules/main/Movie/Movie.tsx
+++ src/components/modules/main/Movie/Movie.tsx
@@ -72,13 +72,13 @@ function Movie({
           />
           <div className="flex !text-white text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible duration-100 opacity-0 invisible justify-end flex-col absolute inset-0 movie-overlay rounded-md">
             <div className="pb-6 px-2 space-y-2 md:space-y-3">
-              <p className="text-xs md:text-[13px]">
+              <p className="text-xs md:text-[13px] !text-white">
                 {type === "film" ? "فیلم" : "سریال"} -{" "}
-                <span className="font-Dana">{showTime}</span>
+                <span className="font-Dana text-white">{showTime}</span>
               </p>
               <div className="flex text-xs md:text-[13px] gap-x-1">
-                <Heart />
-                <span className="font-Dana mt-[1px]">%80</span>
+                <Heart className="text-white" />
+                <span className="font-Dana mt-[1px] !text-white">%80</span>
               </div>
             </div>
           </div>
@@ -106,13 +106,13 @@ function Movie({
           />
           <div className="flex text-xs md:text-sm transition-all group-hover:opacity-100 group-hover:visible duration-100 opacity-0 invisible justify-end flex-col absolute inset-0 movie-overlay rounded-md">
             <div className="pb-6 px-2 space-y-2 md:space-y-3">
-              <p className="text-xs md:text-[13px]">
-                {type === "film" ? "فیلم" : "سریال"} -{" "}
-                <span className="font-Dana">{showTime}</span>
+              <p className="text-xs md:text-[13px] text-white">
+                <span>{type === "film" ? "فیلم" : "سریال"} - </span>
+                <span className="font-Dana text-white">{showTime}</span>
               </p>
               <div className="flex gap-x-1">
                 <Heart />
-                <span className="font-Dana mt-[1px]">%80</span>
+                <span className="font-Dana mt-[1px] text-white">%80</span>
               </div>
             </div>
           </div>
