476ef613a34d1000db5b0c27ee046bc4f2ea12f4
Milad salamian <miladsalami1385@gmail.com>
2026-06-25 19:57:45 +0330

    add image loading in slider component

--- src/components/templates/index/Slider/Slider.tsx
+++ src/components/templates/index/Slider/Slider.tsx
@@ -1,14 +1,22 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import { Autoplay, Navigation } from "swiper/modules";
 
 import Image from "next/image";
 import Link from "next/link";
+
 function Slider({ slides }: any) {
+  const [loadedImages, setLoadedImages] = useState<{ [key: string]: boolean }>(
+    {},
+  );
+
+  const handleImageLoad = (id: string) => {
+    setLoadedImages((prev) => ({ ...prev, [id]: true }));
+  };
+
   return (
     <div className="px-4 md:px-0 mt-5 md:mt-10 relative bottom-[47px]">
       <Swiper
@@ -36,29 +44,44 @@ function Slider({ slides }: any) {
         {[...slides]
           .reverse()
           .slice(0, 15)
-          .map((slide: any) => (
-            <SwiperSlide key={slide._id}>
-              <Link
-                href={`/${slide.type === "film" ? "movie" : "series"}/${
-                  slide.link
-                }`}
-                className="relative"
-              >
-                <Image
-                  src={slide.deskBanner}
-                  alt="slide1.jpg"
-                  width={490}
-                  height={186}
-                  className="rounded-xl h-[150px] md:h-auto"
-                />
+          .map((slide: any) => {
+            const isLoaded = loadedImages[slide._id] || false;
 
-                <div className="absolute inset-0 bg-black/25"></div>
-                <h3 className="absolute z-20 text-white bottom-6 right-6">
-                  {slide.title}
-                </h3>
-              </Link>
-            </SwiperSlide>
-          ))}
+            return (
+              <SwiperSlide key={slide._id}>
+                <Link
+                  href={`/${slide.type === "film" ? "movie" : "series"}/${
+                    slide.link
+                  }`}
+                  className="relative block"
+                >
+                  <div className="relative">
+                    {!isLoaded && (
+                      <div className="w-full h-[150px] md:h-auto bg-gray-700 rounded-xl overflow-hidden relative">
+                        <div className="absolute inset-0 -translate-x-full animate-shimmer bg-gradient-to-r from-transparent via-white/20 to-transparent"></div>
+                      </div>
+                    )}
+
+                    <Image
+                      src={slide.deskBanner}
+                      alt={slide.title}
+                      width={490}
+                      height={186}
+                      onLoad={() => handleImageLoad(slide._id)}
+                      className={`rounded-xl h-[150px] md:h-auto transition-opacity duration-300 ${
+                        isLoaded ? "opacity-100" : "opacity-0"
+                      }`}
+                    />
+                  </div>
+
+                  <div className="absolute inset-0 bg-black/25 rounded-xl"></div>
+                  <h3 className="absolute z-20 text-white bottom-6 right-6">
+                    {slide.title}
+                  </h3>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
       </Swiper>
     </div>
   );
