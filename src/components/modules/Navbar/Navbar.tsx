d2fd75a79c806f0147f9b3f791d14b9604dd8df8
Milad salamian <miladsalami1385@gmail.com>
2026-06-30 02:18:27 +0330

    fix get active profile

--- src/components/modules/Navbar/Navbar.tsx
+++ src/components/modules/Navbar/Navbar.tsx
@@ -15,9 +15,9 @@ import MobileNavbar from "./MobileNavbar";
 import { limitedRoute } from "@/public/db";
 import Menus from "./Menus";
 function Navbar({ user, userSubscription, menus }: any) {
-  const { activeProfile } = useAuth();
+  let { activeProfile } = useAuth();
   const category: any = useCategoryName();
-
+  activeProfile = user?.profiles?.[0];
   const pathname = usePathname();
   const [isShowProfile, setIsShowProfile] = useState(false);
   let navBar = useRef<any>("");
