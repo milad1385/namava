bceb07a09daa2b1fdaa64cc43d30fcd41e651257
Milad salamian <miladsalami1385@gmail.com>
2026-07-01 23:57:07 +0330

    fix mobile menu content bug

--- src/components/modules/Navbar/MobileMenuContent.tsx
+++ src/components/modules/Navbar/MobileMenuContent.tsx
@@ -1,10 +1,12 @@
 import { mobileMenuNavbar, sidebarLinks } from '@/public/db';
 import { usePathname } from 'next/navigation';
 import MobileMenu from './MobileMenu';
+import { useAuth } from '@/src/context/AuthContextProvider';
 
 function MobileMenuContent() {
     const pathname = usePathname();
     const isUserPanel = pathname.includes("/p-user");
+     const { isLogin } = useAuth();
   return (
     <div className="my-5 border-b-2 border-b-gray-600 pb-5">
           <ul className="text-white space-y-6">
@@ -13,7 +15,7 @@ function MobileMenuContent() {
                   <MobileMenu {...menu} key={menu.id}/>
                 ))
               : mobileMenuNavbar.map((menu) => (
-                   <MobileMenu {...menu} key={menu.id}/>
+                   <MobileMenu {...menu} isLogin={isLogin} key={menu.id}/>
                 ))}
           </ul>
         </div>
