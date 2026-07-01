75563e9211bc4e34483f986fbe7f9b2714c6b9d2
Milad salamian <miladsalami1385@gmail.com>
2026-07-01 23:56:59 +0330

    fix mobile menu bug

--- src/components/modules/Navbar/MobileMenu.tsx
+++ src/components/modules/Navbar/MobileMenu.tsx
@@ -2,12 +2,22 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-function MobileMenu({ link, icon, title }) {
+function MobileMenu({
+  link,
+  icon,
+  title,
+  isLogin,
+}: {
+  link: string;
+  icon: React.ReactNode;
+  title: string;
+  isLogin?: boolean;
+}) {
   const pathname = usePathname();
   return (
     <li>
       <Link
-        href={title === "کودکان" ? "/profile-list" : link}
+        href={title === "کودکان" && isLogin ? "/profile-list" : link}
         className={`flex items-center gap-x-3 text-lg ${
           pathname === link ? "active" : ""
         }`}
