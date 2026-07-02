3bf171f0b68bf305dfa6b410cfd30ce5c5f08ccd
Milad salamian <miladsalami1385@gmail.com>
2026-07-02 17:57:51 +0330

    fix slider title bug

--- src/components/modules/main/MovieSlider/SiderTitle.tsx
+++ src/components/modules/main/MovieSlider/SiderTitle.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
-import { FaChevronLeft } from "react-icons/fa6";
+import { FiChevronLeft } from "react-icons/fi";
 
 interface SliderTitle {
   title: string;
@@ -11,19 +10,14 @@ function SiderTitle({ title, link }: SliderTitle) {
   const pathname = usePathname();
   const textColor = pathname.includes("/kids") ? "text-black" : "text-white";
   return (
-    <div className="flex items-center gap-x-4 group md:cursor-pointer">
-      <h1 className={`font-IranMedium text-base md:text-lg ${textColor}`}>
-        {title}
-      </h1>
-      {link && (
-        <Link
-          href={link}
-          className="opacity-0 invisible group-hover:visible group-hover:opacity-100 flex items-center gap-x-2 text-[13px] transition-all duration-200"
-        >
-          مشاهده همه
-          <FaChevronLeft className="text-[13px]" />
-        </Link>
-      )}
+    <div className="flex items-center gap-x-2 group md:cursor-pointer">
+      <Link href={link}>
+        <h1 className={`font-IranMedium text-base md:text-lg ${textColor}`}>
+          {title}
+        </h1>
+      </Link>
+
+      <FiChevronLeft className="text-lg md:text-xl" />
     </div>
   );
 }
