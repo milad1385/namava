52c3845cb3bc4d63cce8a5a0ce2f30d4d31de4d2
Milad salamian <miladsalami1385@gmail.com>
2026-07-02 17:58:17 +0330

    add all movies page

---
+++ src/app/(main)/movie/all/[category]/page.tsx
@@ -0,0 +1,36 @@
+import MovieSort from "@/src/components/modules/main/Movie/MovieSort";
+import MovieSlide from "@/src/components/modules/main/MovieSlider/MovieSlide";
+import { getMoviesByCategory } from "@/src/libs/service/services";
+import { TParams } from "@/src/libs/types";
+import { notFound } from "next/navigation";
+import React from "react";
+
+async function page({ params, searchParams }: TParams) {
+  const movies = await getMoviesByCategory(
+    params.category as string,
+    searchParams.status,
+  );
+  if (!movies) {
+    notFound();
+  }
+  return (
+    <div className="my-24 md:my-28 text-white container">
+      <div className="flex items-center justify-between">
+        <h1 className="text-base md:text-xl">
+          <span className="hidden md:inline-block"> تمام فیلم و سریال </span>{" "}
+          <span>
+            {movies[0]?.category?.parrent?.title} - {movies[0]?.category?.title}
+          </span>
+        </h1>
+        <MovieSort />
+      </div>
+      <div className="mt-10 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-5">
+        {movies.map((movie: any) => (
+          <MovieSlide movie={JSON.parse(JSON.stringify(movie))} isMain />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default page;
