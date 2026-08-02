4f08688d24f4b83ab33da9156f00e5fbbd34ddc2
Milad salamian <miladsalami1385@gmail.com>
2026-08-02 19:15:04 +0330

    use edit article form in edit article page p-admin

--- src/app/p-admin/articles/[link]/page.tsx
+++ src/app/p-admin/articles/[link]/page.tsx
@@ -1,14 +1,24 @@
 import Title from "@/src/components/modules/p-admin/Title";
 import EditArticle from "@/src/components/templates/p-admin/article/EditArticle";
-import { getAllMoviesWithOutPagination } from "@/src/libs/service/services";
+import {
+  getAllMoviesWithOutPagination,
+  getArticle,
+} from "@/src/libs/service/services";
+import { TParams } from "@/src/libs/types";
 import React from "react";
 
-async function page() {
-  const movies = await getAllMoviesWithOutPagination();
+async function page({ params }: TParams) {
+  const [movies, article] = await Promise.all([
+    getAllMoviesWithOutPagination(),
+    getArticle(params.link as string),
+  ]);
   return (
     <div>
-      <Title name={`ویرایش مقاله تست`} />
-      <EditArticle movies={JSON.parse(JSON.stringify(movies))} />
+      <Title name={`ویرایش مقاله ${article.title}`} />
+      <EditArticle
+        movies={JSON.parse(JSON.stringify(movies))}
+        article={JSON.parse(JSON.stringify(article))}
+      />
     </div>
   );
 }
