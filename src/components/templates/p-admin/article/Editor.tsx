edb9dfb547775a0c6726d0099b0c5256b3c161b9
Milad salamian <miladsalami1385@gmail.com>
2026-08-02 17:48:26 +0330

    fix editor component style bug

--- src/components/templates/p-admin/article/Editor.tsx
+++ src/components/templates/p-admin/article/Editor.tsx
@@ -1,8 +1,16 @@
 "use client";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import { useEffect, useState } from "react";
+import EditorSkeleton from "./EditorSkeleton";
 
-function Editor({article , onArticle} : any) {
+function Editor({ article, onArticle }: any) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const uploadConfig = {
     ckfinder: {
       uploadUrl: "http://localhost:3001/upload",
@@ -16,8 +24,12 @@ function Editor({article , onArticle} : any) {
     direction: "rtl",
   };
 
+  if (!isClient) {
+    return <EditorSkeleton />;
+  }
+
   return (
-    <div className="editor-container">
+    <div className="[&_.ck-editor__editable]:min-h-[350px] [&_.ck-editor__main]:h-auto [&_.ck-content]:min-h-[350px] w-full">
       <CKEditor
         editor={ClassicEditor as any}
         config={uploadConfig}
@@ -25,6 +37,14 @@ function Editor({article , onArticle} : any) {
         onChange={(event, editor) => {
           const data = editor.getData();
           onArticle(data);
+        }}
+        onReady={(editor) => {
+          const editable = editor.editing.view.document.getRoot();
+          if (editable) {
+            editor.editing.view.change((writer) => {
+              writer.setStyle("min-height", "350px", editable);
+            });
+          }
         }}
       />
     </div>
